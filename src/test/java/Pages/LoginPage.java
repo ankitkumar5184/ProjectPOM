5e936dcefbfe94bd241c10505de152798aa5a11f
AnkitKumar <ankit.kumar@infostride.com>
2022-02-08 19:48:41 +0530

    LoginPage

--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -1,9 +1,12 @@
 package Pages;
 
 import TestBase.testBase;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -12,52 +15,49 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Properties;
 
 import static TestBase.testBase.driver;
 
 public class LoginPage extends testBase {
-    String path,uname,pass,message;
+    public static Properties prop;
+    public static WebDriver driver;
+    //String path,uname,pass,message;
+
     @FindBy(id="txtUsername")
     WebElement username;
+
     @FindBy(id="txtPassword")
     WebElement userpass;
+
     @FindBy(id="btnLogin")
     WebElement loginButton;
+
     @FindBy(linkText = "Forgot your password?")
     WebElement forgetPass;
 
-    public LoginPage()
+    public LoginPage (WebDriver driver)
     {
+        this.driver =driver;
         PageFactory.initElements(driver,this);
     }
     public String VerifyTitle() {
         return driver.getTitle();
     }
     public void login() throws IOException {
-        path = System.getProperty("user.dir")+ "//ExcelSheet//loginData.xlsx";
-        System.out.println(path);
-        FileInputStream fis = null;
-        try{
-            fis=new FileInputStream(path);
-        }catch (FileNotFoundException e){
-            e.printStackTrace();
-        }
-        XSSFWorkbook wb = new XSSFWorkbook(fis);
-        XSSFSheet sheet = wb.getSheet("Login_Details");
-        System.out.println(sheet.getLastRowNum());
-        for (int i = 1; i <= sheet.getLastRowNum(); i++) {
-            uname = sheet.getRow(i).getCell(0).getStringCellValue();
-            username.sendKeys(uname);
-            pass = sheet.getRow(i).getCell(1).getStringCellValue();
-            userpass.sendKeys(pass);
-            loginButton.click();
+        String path = System.getProperty("user.dir");
+        FileInputStream fis = new FileInputStream(path+"//TestData//loginData.xls");
+        System.out.println("Hello");
+        HSSFWorkbook wb = new HSSFWorkbook(fis);
+        HSSFSheet sheet = wb.getSheetAt(0);
+        //System.out.println(sheet.getLastRowNum());
+        String uname = sheet.getRow(1).getCell(0).getStringCellValue();
+        String pass = sheet.getRow(1).getCell(1).getStringCellValue();
 
+        username.sendKeys(uname);
+        userpass.sendKeys(pass);
+        loginButton.click();
 
-
-//        username.sendKeys(prop.getProperty("username"));
-//        userpass.sendKeys(prop.getProperty("password"));
-//        loginButton.click();
-        }
     }
     public void forgetpas(){
         forgetPass.click();
